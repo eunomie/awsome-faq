2a4929f2f8e899dad9522f08338456807a969526
Yves Brissaud <yves.brissaud@gmail.com>
2016-02-25 07:05:17 +0100

    Add slug

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-Question.create(title: 'How to have a clean history with git?')
-Question.create(title: 'How to determine whether my calculation of pi is accurate?')
+Question.create(title: 'How to have a clean history with git?', slug: 'how-to-have-a-clean-history-with-git')
+Question.create(title: 'How to determine whether my calculation of pi is accurate?', slug: 'how-to-determine-whether-my-calculation-of-pi-is-accurate')
