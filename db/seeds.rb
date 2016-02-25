8ec4be1a5ab3aaa58f0edbef1034791d230edc4a
Yves Brissaud <yves.brissaud@gmail.com>
2016-02-25 06:43:53 +0100

    Add seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Question.create(title: 'How to have a clean history with git?')
+Question.create(title: 'How to determine whether my calculation of pi is accurate?')
